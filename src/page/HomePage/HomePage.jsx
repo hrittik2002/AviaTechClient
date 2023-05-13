21c0727f302223c8c0a30fd12e8cdc1c0523d2fc
13KuntalRoy <roykuntal1213@gmail.com>
2023-05-13 08:52:39 +0530

    login

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -1,52 +1,75 @@
 import React, { useState } from "react";
-import styles from './HomePage.module.css'
+import styles from "./HomePage.module.css";
 import {
     Modal,
     ModalOverlay,
     ModalContent,
     ModalHeader,
-    ModalFooter,
+
     ModalBody,
-    ModalCloseButton,
+
     Button,
-} from '@chakra-ui/react'
+    FormControl,
+    FormLabel,
+    Input,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
     const [showModal, setShowModal] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        // handle login logic
+        console.log("Username:", username);
+        console.log("Password:", password);
+        setShowModal(false);
+    };
 
     return (
         <div>
-            { /* Navbar */}
+            {/* Navbar */}
             <div className={styles.navContainer}>
                 <div className={styles.logoContainer}>AviaTech</div>
                 <div className={styles.navItems}>
-                    <Link className={styles.navItem} onClick={() => setShowModal(true)} to="/">
-                        login
-                    </Link>
+                    <div className={styles.navItem} onClick={() => setShowModal(true)}>
+                        Login
+                    </div>
                     <Link className={styles.navItem} to="/search">
-                        register
+                        Register
                     </Link>
                 </div>
             </div>
             {/* Modal */}
-            <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+            <Modal isOpen={showModal} onClose={() => setShowModal(false)} isCentered className={styles.modal}>
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        hi
+                <ModalContent className={styles.modalDialog}>
+                    <ModalHeader className={styles.modalHeader}>
+                        <div className={styles.modalTitle}>Login</div>
+                        <button className={styles.modalClose} onClick={() => setShowModal(false)}>
+                            &times;
+                        </button>
+                    </ModalHeader>
+                    <ModalBody className={styles.modalBody}>
+                        <form onSubmit={handleLogin}>
+                            <FormControl>
+                                <FormLabel>Username</FormLabel>
+                                <Input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+                            </FormControl>
+                            <FormControl >
+                                <FormLabel>Password</FormLabel>
+                                <Input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                            </FormControl>
+                            <Button  type="submit" className={styles.modalButton}>
+                                Login
+                            </Button>
+                        </form>
                     </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={() => setShowModal(false)}>
-                            Close
-                        </Button>
-                        <Button variant='ghost'>Secondary Action</Button>
-                    </ModalFooter>
                 </ModalContent>
             </Modal>
+
         </div>
     );
 };
